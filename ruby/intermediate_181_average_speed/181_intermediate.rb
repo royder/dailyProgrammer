3e3804fffd9c0ff6c1f21c9144b1b9b2718559c7
royder <royder@users.noreply.github.com>
2015-04-26 16:43:01 -0500

    Update challenge int181

--- ruby/intermediate_181_average_speed/181_intermediate.rb
+++ ruby/intermediate_181_average_speed/181_intermediate.rb
@@ -1,9 +1,11 @@
 # intermediate_181 average speed
 # http://www.reddit.com/r/dailyprogrammer/comments/2hcwzn/09242014_challenge_181_intermediate_average_speed/
 
+require 'time'
+
 def file_usable?(filepath)
   return false unless File.exists?(filepath)
-  #return false unless File.readable?(filepath)
+  return false unless File.readable?(filepath)
   return true
 end
 
@@ -15,27 +17,35 @@ if !file_usable?(filepath)
   abort("File (#{filepath}) does not exist. Aborting...")
 end
 
-File.open(filepath, 'r') {|file|
-  file.each_line {|line|
-    cameras_distances = Hash.new
-    vehicles = Hash.new{|h,k| h[k]=[]}
-    case
-    when line.start_with?('Speed limit')
-      /Speed limit is (?<speed_limit>[1-9][0-9]\.[0-9]{2}) (?<speed_unit>mph|kmh)\./ =~ line
-      puts speed_limit
-      puts speed_unit
-    when line.start_with?('Speed camera number')
-      /Speed camera number (?<camera_num>[1-9]) is (?<camera_distance>0|[1-9][0-9]{0,3}) metres down the motorway\./ =~ line
-      cameras_distances[camera_num] = camera_distance.to_i
-      puts cameras_distances.inspect
-    when line.start_with?('Start of log for')
-      next 
-    when line.start_with?('Vehicle')
-      /Vehicle (?<vehicle_id>[0-9A-Z]{1,4} [0-9A-Z]{1,4}) passed camera (?<camera_num>[1-9]) at (?<time>[0-9]{2}:[0-9]{2}:[0-9]{2})\./ =~ line
-      vehicles[vehicle_id.gsub(/\s/,'')] << [camera_num, time]
-      puts vehicles.inspect
+vehicles = Hash.new {|h,k| h[k]=[]}
+cameras_distances = Hash.new
+speed_limit = nil
+
+File.open(filepath, 'r').each {|line|
+  case
+  when line.start_with?('Vehicle')
+    /Vehicle (?<vehicle_id>[0-9A-Z]{1,4} [0-9A-Z]{1,4}) passed camera (?<camera_num>[1-9]) at (?<camera_time>[0-9]{2}:[0-9]{2}:[0-9]{2})\./ =~ line
+    if vehicles.has_key?(vehicle_id)
+      distance = (cameras_distances[camera_num] - vehicles[vehicle_id][1]) * 0.00062137
+      time = (Time.parse(camera_time) - Time.parse(vehicles[vehicle_id][0])) / 60 / 60
+      speed = distance / time
+      puts "Vehicle #{vehicle_id} broke the speed limit by #{(speed - speed_limit).round(1)} mph." if speed > speed_limit
+      vehicles[vehicle_id] = [camera_time, cameras_distances[camera_num]]
     else
-      puts 'no match'
+      vehicles[vehicle_id] = [camera_time, cameras_distances[camera_num]]
     end
-  }
+  when line.start_with?('Speed limit')
+    # Assigns speed_limit and speed_unit
+    /Speed limit is (?<speed_limit>[1-9][0-9]\.[0-9]{2}) (?<speed_unit>mph|kmh)\./ =~ line
+    speed_limit *= 0.62137 if speed_unit == 'kmh'
+    speed_limit = speed_limit.to_i
+  when line.start_with?('Speed camera number')
+    # Assigns camera_num and camera_distance
+    /Speed camera number (?<camera_num>[1-9]) is (?<camera_distance>0|[1-9][0-9]{0,3}) metres down the motorway\./ =~ line
+    cameras_distances[camera_num] = camera_distance.to_i
+  when line.start_with?('Start of log for')
+    next 
+  else
+    abort("Could not process the following file line: #{line}). Aborting...")
+  end
 }
